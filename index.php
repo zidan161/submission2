f3e25679f70a2e3f533501857047d8d2f0c9c547
zidanaqilamuhammad <38450460+zidanaqilamuhammad@users.noreply.github.com>
2019-05-01 11:40:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload File</title>
+<style>
+table {
+  width:100%;
+}
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+  text-align: left;
+}
+</style>
+</head>
+<body>
+    <h1> Upload File! </h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        Pilih file: <input type="file" name="berkas"/>
+        <input type="submit" name="submit" value="upload"/>
+        <br/>
+        <p><button type="submit" name="list">List</button></p>
+    </form>
+    <?php
+        require_once 'vendor/autoload.php';
+        require_once "./random_string.php";
+
+        use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+        use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+        use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+        use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+        use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
+   
+        $containerName = "blockblobs";
+
+        // Membuat blob client.
+        $blobClient = BlobRestProxy::createBlobService($connectionString);
+
+        if (isset($_POST['submit']) && $_POST['submit'] == 'upload') {
+
+            if (isset($_FILES['berkas'])) {
+
+                $fileName = $_FILES['berkas']['name'];
+                $fileDirectory = $_FILES['berkas']['tmp_name'];
+                $fileToUpload = $fileName;
+                move_uploaded_file($fileDirectory,"image/$fileName");
+ 
+               try {
+
+                    // Getting local file so that we can upload it to Azure
+                    $myfile = fopen("image/$fileName", "r") or die("Unable to open file!");
+                    fclose($myfile);
+        
+                    # Upload file as a block blob
+                    echo "Uploading BlockBlob: ".PHP_EOL;
+                    echo $fileToUpload;
+                    echo "<br />";
+            
+                    $content = fopen("image/$fileName", "r");
+
+                    //Upload blob
+                    $blobClient->createBlockBlob($containerName, $fileName, $content);
+
+                }
+                catch(ServiceException $e){
+                    // Handle exception based on error codes and messages.
+                    // Error codes and messages are here:
+                    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                    $code = $e->getCode();
+                    $error_message = $e->getMessage();
+                    echo $code.": ".$error_message."<br />";
+                }
+                catch(InvalidArgumentTypeException $e){
+                    // Handle exception based on error codes and messages.
+                    // Error codes and messages are here:
+                    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                    $code = $e->getCode();
+                    $error_message = $e->getMessage();
+                    echo $code.": ".$error_message."<br />";
+                }
+            }
+          else echo "ERROR";
+       }
+       
+       if (isset($_POST['list'])) {
+           // List blobs.
+           $listBlobsOptions = new ListBlobsOptions();
+
+           do{
+               echo "<table>
+                     <tr>
+                        <th>Name</th>
+                        <th>Url</th>
+                     </tr>";
+               $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+             
+               foreach ($result->getBlobs() as $blob)
+               {
+                 $name = $blob->getName();
+                 $url = $blob->getUrl();
+                 echo "<form action='vision.php' method='post' enctype='multipart/form-data'>
+                        <tr>
+                          <td>$name</td>
+                          <td>$url</td>
+                          <td><button type='submit' name='button' value='$url'/>Analize!</td>
+                        </tr>
+                       </form>";
+               }
+               echo "</table>";
+        
+               $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+           } while($result->getContinuationToken());
+          echo "<br/>";
+       }    
+    ?>
+</body>
+</html>
